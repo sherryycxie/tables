5da618df010ff1b163989bd2e727b5b480154223
Sherry Xie <ycxie25@gmail.com>
2026-01-18 09:26:29 -0800

    Initial Commit

---
+++ tables/tablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tablesApp.swift
+//  tables
+//
+//  Created by Sherry Xie on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct tablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
